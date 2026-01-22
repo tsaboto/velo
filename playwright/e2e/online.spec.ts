0765a8e2001fc5512280279d26a2093b30001d86
tati <tsaboto@gmail.com>
2026-01-22 12:17:33 -0500

    chaging the scenario name for the correct name

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('the website is online and the title is correct', async ({ page }) => {
+test('webapp is online and the title is correct', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
