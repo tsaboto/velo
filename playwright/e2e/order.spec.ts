bd123913af362ba006668571093e2acebc41409f
tati <tsaboto@gmail.com>
2026-01-22 16:55:02 -0500

    AAA - Arrange, Act, Assert

--- playwright/e2e/order.spec.ts
+++ playwright/e2e/order.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test'
+///AAA - Arrange, Act, Assert
 
-test('test', async ({ page }) => {
+test('the order lookup page is loaded and the order is found', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
-
-  //checkpoint 1: the webapp is online and the title is correct
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //checkpoint 2: the order lookup page is loaded
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  await page.getByTestId('search-order-id').fill('VLO-NJJ3IX')
   
+  //Act
+  await page.getByTestId('search-order-id').fill('VLO-NJJ3IX')
   await page.getByTestId('search-order-button').click()
+  
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-NJJ3IX')
 
