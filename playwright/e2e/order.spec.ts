e500e31e1c4b3a2fe361753189f177147d8c00d7
tati <tsaboto@gmail.com>
2026-01-25 17:16:51 -0500

    first challenge

--- playwright/e2e/order.spec.ts
+++ playwright/e2e/order.spec.ts
@@ -2,23 +2,38 @@ import { test, expect } from '@playwright/test'
 ///AAA - Arrange, Act, Assert
 
 test('the order lookup page is loaded and the order is found', async ({ page }) => {
-  //Arrange
-  await page.goto('http://localhost:5173/')
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    //Arrange
+    const orderId = 'VLO-NJJ3IX';
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  
-  //Act
-  await page.getByTestId('search-order-id').fill('VLO-NJJ3IX')
-  await page.getByTestId('search-order-button').click()
-  
-  //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-NJJ3IX')
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-    
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+    //Act
+    //   await page.getByTestId('search-order-id').fill('VLO-NJJ3IX') //data-testid
+    //   await page.locator('//label[text()="Número do Pedido"]/..//input').fill('VLO-NJJ3IX'); //XPATH
+    //   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-NJJ3IX'); //role and name CSS
+    await page.getByLabel('Número do Pedido').fill(orderId); //label and name CSS
+
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    //Assert
+
+    const orderIdText = page.getByText(orderId, { exact: true });
+    await expect(orderIdText).toBeVisible({ timeout: 10_000 });
+
+
+    //simples
+    // await expect(orderIdText.locator('..').locator('..').locator('..')).toContainText('APROVADO');
+
+    //elaborada
+    const orderCard = page
+        .locator('div.rounded-lg.border.bg-card')
+        .filter({ hasText: orderId });
+
+    await expect(orderCard).toContainText('APROVADO');
+
 
 })
